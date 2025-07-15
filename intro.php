ab334842e8140698e18b8a181bf373de0522873a
dumakude <dumakudes@hotmail.com>
2025-07-15 14:34:50 +0200

    day 2

--- intro.php
+++ intro.php
@@ -1,51 +1,16 @@
-<?php
-
-//our code /
-$marks = 98;
-$students = array("Mateyu", "Philipo", "Moses", "John", "Jane");
-
-
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel Project</title>
+    <title>Day2</title>
 </head>
 <body>
-    
-
-<h1>About User </h1>
-<p> 
-    <?php
-    if($marks>80){
-        echo "Distinction<br>";
-    }elseif($marks>=70){ 
-        echo "This is a Credit";
-    }else{ 
-        echo "This is a Fail";
-    }
-
-    foreach($students as $index => $name) {
-        echo "Student $index: $name<br>";
-    }
-    
-
-echo "<table border='1'>";
-    echo "<tr><th>Index</th><th>Name</th></tr>";
-
-    foreach($students as $key => $val){
-        echo "<tr><td>" . ($key+1) . "</td> <td>" . $val . "</td></tr>";
-    }
-    echo "</table>";
-
-    ?>
-</p>
-
-
+    <form action="submit.php" method="post">
+        <label for="intro">Personal Information:</label><br>
+        <input type="text" name="username" required><br>
+        <input type="number" name="age" required><br>
+        <input type="email" name="email" required><br>
+        <button type="submit">Submit</button><br>
 </body>
 </html>
---
+++ submit.php
@@ -0,0 +1,7 @@
+<?php
+
+$name = $_POST['username'];
+$age = $_POST['age'];
+$email = $_POST['email'];
+echo "Your name is $name and your age is $age so your email should be $email "; 
+
